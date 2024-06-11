49a4707c6bd53671fbc9ef800b3ea82db070c81e
unknown <vknwl077@gmail.com>
2024-06-12 01:35:25 +0530

    second

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -37,15 +37,16 @@ export const createProductCtrl = asyncHandler(async (req, res) => {
 });
 
 // 2
-// @desc GET ALL PRODUCTS
-// @ROUTE GET  /api/v1/products
-// @access PUBLIC
-
+// @desc    Get single product
+// @route   GET /api/products/:id
+// @access  Public
 export const getProductsCntrl = asyncHandler(async (req, res) => {
   // const aux = await Product.find();
   // console.log(aux);
+  // return the document
   console.log(req.query);
   let productQuery = Product.find({});
+  // return the Query object with other fields also
   // console.log(productQuery);
   // searn by name if exist otherwise return the empty strig
   // 1-FILTER SEARCH BY NAME
@@ -93,10 +94,108 @@ export const getProductsCntrl = asyncHandler(async (req, res) => {
   // await the query
   // put it below to the search by name otherwise it return  the products because if
   // statement is not run but this statement if run above it stores all the products
-
+  // PAGINATION
+  // PAGE
+  const page = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
+  // limit
+  const limit = parseInt(req.query.limit) ? parseInt(req.query.limit) : 10;
+  // startIndex
+  const startIndex = (page - 1) * limit;
+  // end Index
+  const lastIndex = page * limit;
+  // totoal
+  const total = await Product.countDocuments();
+  //
+  productQuery = productQuery.skip(startIndex).limit(limit);
   const products = await productQuery;
+  // pagination result
+  const pagination = {};
+  if (lastIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
   res.json({
     status: 'success',
+    total,
+    results: products.length,
+    pagination,
+    message: 'Products fetched successfully',
     products,
+  });
+});
+
+// 3
+// @desc    Get all products
+// @route   GET /api/v1/products
+// @access  Public
+export const getProductCntrl = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    throw new Error('Product Not Found');
+  }
+  res.json({
+    status: 'success',
+    message: 'Product fetched successfullly',
+    product,
+  });
+});
+
+// 4
+// @desc    update  product
+// @route   PUT /api/products/:id/update
+// @access  Private/Admin
+export const updateProductCntrl = asyncHandler(async (req, res) => {
+  const {
+    name,
+    description,
+    category,
+    sizes,
+    colors,
+    user,
+    price,
+    totalQty,
+    brand,
+  } = req.body;
+  // taking the field from the input
+  // validation
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+      description,
+      category,
+      sizes,
+      colors,
+      user,
+      price,
+      totalQty,
+      brand,
+    },
+    { new: true, runValidators: true }
+  );
+  res.json({
+    status: 'success',
+    message: 'Product updated successfully',
+    product,
+  });
+});
+
+// 5
+// @desc    delete  product
+// @route   DELETE /api/products/:id/delete
+// @access  Private/Admin
+export const deleteProductCntrl = asyncHandler(async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.json({
+    status: 'success',
+    message: 'Product deleted successfully',
   });
 });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import {
   createProductCtrl,
+  deleteProductCntrl,
+  getProductCntrl,
   getProductsCntrl,
+  updateProductCntrl,
 } from '../controllers/productControllers.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 const productRouter = express.Router();
 productRouter.post('/', isLoggedIn, createProductCtrl);
-productRouter.get('/', isLoggedIn, getProductsCntrl);
+productRouter.get('/', getProductsCntrl);
+productRouter.get('/:id', getProductCntrl);
+productRouter.put('/:id', isLoggedIn, updateProductCntrl);
+productRouter.delete('/:id/delete', isLoggedIn, deleteProductCntrl);
 export default productRouter;
